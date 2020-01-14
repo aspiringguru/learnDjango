03f8daf2ce3df1123f8ea133f8d6f8245d48e88a
aspiringguru <bmatthewtaylor@gmail.com>
2020-01-14 16:44:45 +1000

    wip, end tutorial 02

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+
+admin.site.register(Question)
+#import & register class Question defined in the app model file
+#file polls/models.py has > class Question(models.Model):
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+#https://docs.djangoproject.com/en/2.2/topics/db/models/
+import datetime
+from django.utils import timezone
+
 
 # Create your models here.
+class Question(models.Model):
+    #Question has a question and a publication date.
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def my_dumb_method(self):
+        return "this is my dumb method."
+
+
+class Choice(models.Model):
+    #Each Choice is associated with a Question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    #Choice has two fields: the text of the choice and a vote tally.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
