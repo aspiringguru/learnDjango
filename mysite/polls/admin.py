f06619c9a82157eec60e077138880b057acf73ad
aspiringguru <bmatthewtaylor@gmail.com>
2020-01-15 17:55:54 +1000

    part 7, inline class in form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice  #Choice class as imported from polls/models.py
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+    #fields = ['question_text', 'pub_date']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    #add a bunch of Choices directly when you create the Question object.
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
 #import & register class Question defined in the app model file
 #file polls/models.py has > class Question(models.Model):
